faa5a11178a0aa4afb2b92f3c39c89d88e1e2618
Paul Tannenbaum <paultannenbaum@gmail.com>
2013-08-15 19:02:37 -0700

    styling the shows#index page

--- themes/Korduroy/archive-shows.php
+++ themes/Korduroy/archive-shows.php
@@ -1,33 +1,46 @@
 <?php get_header(); ?>
 <?php load_template(TEMPLATEPATH . '/partials/shows-sub-nav.php'); ?>
 
-<div id="body" class="supporters-page">
-  <section id="main" role="main">
-    <h1>Old</h1>
-
+<div id="body" class="shows-page shows-index">
+  <section class="main" role="main">
     <?php $categories = get_terms('show_category', array(
-      'hide_empty' => 0
-    ));
+      'hide_empty' => 0,
+      'orderby' => 'count',
+      'order' => 'DESC'
+      ));
     foreach( $categories as $category ):
     ?>
-      <section class="category-<?php echo $category -> name?>">
-        <h2><a href="<?php echo get_term_link($category, $category -> taxonomy); ?>"><?php echo $category -> name; ?></a></h2>
-        <p class="description"><?php echo $category -> description ?></p>
+      <section class="show-channel">
+        <header class="channel-header">
+          <a class="channel-link" href="<?php echo get_term_link($category, $category -> taxonomy); ?>">
+            <span class="channel-icon <?php echo $category -> slug ?>"></span>
+            <p class="channel-info"><?php echo $category -> description ?></p>
+          </a>
+        </header>
 
-        <ul class="shows-list">
-          <?php $posts = get_posts(array(
-            'post_type' => 'shows',
-            'taxonomy' => $category->taxonomy,
-            'term' => $category->slug,
-            'nopaging' => true, // to show all posts in this category, could also use 'numberposts' => -1 instead
-          ));
-          foreach($posts as $post): // begin cycle through posts of this category
-          setup_postdata($post); //set up post data for use in the loop (enables the_title(), etc without specifying a post ID)
-          ?>
-            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-          <?php endforeach; ?>
-        </ul>
+        <div class="channel-list-container">
+          <ul class="channel-list">
+            <?php $posts = get_posts(array(
+              'post_type' => 'shows',
+              'taxonomy' => $category->taxonomy,
+              'term' => $category->slug,
+              'nopaging' => true, // to show all posts in this category, could also use 'numberposts' => -1 instead
+            ));
+            foreach($posts as $post): // begin cycle through posts of this category
+            setup_postdata($post); //set up post data for use in the loop (enables the_title(), etc without specifying a post ID)
+            ?>
+              <li class="episode">
+                <a class="episode-link" href="<?php the_permalink(); ?>">
+
+                  <img class="episode-thumb" src="http://b.vimeocdn.com/ts/440/907/440907059_200.jpg" />
+                  <span class="episode-title"><?php the_title(); ?></span>
+                </a>
+              </li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
       </section>
+      <hr class="channel-separator" />
     <?php endforeach; ?>
   </section>
 </div>
--- themes/Korduroy/partials/shows-sub-nav.php
+++ themes/Korduroy/partials/shows-sub-nav.php
@@ -1,7 +1,13 @@
 <nav id="ktv-sub-nav">
   <h1 class="sub-nav-heading">Shows</h1>
   <ul class="sub-nav-list">
-    <?php wp_list_categories(array('hide_empty' => 0, 'title_li' => __(''), 'taxonomy' => 'show_category')); ?>
+    <?php wp_list_categories(array(
+      'taxonomy' => 'show_category',
+      'hide_empty' => 0,
+      'title_li' => '',
+      'orderby' => 'count',
+      'order' => 'DESC'
+    )); ?>
   </ul>
   <span class="separator-container">
     <hr class="horizontal-stitch" />
