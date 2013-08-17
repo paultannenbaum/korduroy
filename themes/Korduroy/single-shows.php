11edfb9646b44061e962f7fbd6e9b217b0c99b97
Paul Tannenbaum <paultannenbaum@gmail.com>
2013-08-16 18:36:00 -0700

    added some logic to single shows

--- themes/Korduroy/single-shows.php
+++ themes/Korduroy/single-shows.php
@@ -1,20 +1,28 @@
 <?php get_header(); ?>
+<?php load_template(TEMPLATEPATH . '/partials/shows-sub-nav.php'); ?>
 
 <div id="body" class="supporters-page">
 
-  <h1>testing</h1>
-
-
-  <?php
-  // wp_list_categories('hide_empty=0&taxonomy=show_category');
-  ?>
-
   <section class="main" role="main">
+
     <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php if(get_field('vimeo_id')): ?>
+        <p><?php the_field('vimeo_id'); ?></p>
+        <p>Vimeo</p>
+      <?php elseif(get_field('youtube_id')): ?>
+        <p><?php the_field('youtube_id'); ?></p>
+        <p>Youtube</p>
+      <?php endif; ?>
+
+
+      <div class="video-wrapper">
+        <iframe src="http://player.vimeo.com/video/<?php echo $vimeo_id; ?>?title=0&amp;byline=0&amp;portrait=0&amp;color=cfff66" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+      </div>
+
       <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
       <p><?php the_field('description'); ?></p>
-      <p><?php the_field('vimeo_id'); ?></p>
-      <p><?php the_field('youtube_id'); ?></p>
+
       <p><?php the_field('thumbnail'); ?></p>
 
       <?php if(get_field('credits')): ?>
@@ -28,13 +36,10 @@
       <?php endif; ?>
     <?php endwhile; ?>
 
+    <!--<div class="comments-wrapper">-->
+      <!--<?php comments_template(); ?>-->
+    <!--</div>-->
 
-    <h1>categories</h1>
-
-
-    <div class="comments-wrapper">
-      <?php comments_template(); ?>
-    </div>
   </section>
 </div>
 
