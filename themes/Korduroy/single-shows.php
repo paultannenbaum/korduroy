37d8073b02cfd98bd7702c3ca5a435e71ed1ab60
Paul Tannenbaum <paultannenbaum@gmail.com>
2013-08-19 22:01:49 -0700

    more work on shows#show page

--- themes/Korduroy/single-shows.php
+++ themes/Korduroy/single-shows.php
@@ -1,11 +1,12 @@
 <?php get_header(); ?>
 <?php load_template(TEMPLATEPATH . '/partials/shows-sub-nav.php'); ?>
 
-<div id="body" class="supporters-page">
+<div id="body" class="shows-page show-page-show">
 
   <section class="main" role="main">
 
     <?php while ( have_posts() ) : the_post(); ?>
+
 
       <div class="video-wrapper">
         <?php if(get_field('vimeo_id')): ?>
@@ -14,27 +15,33 @@
           <iframe src="//www.youtube.com/embed/<?php the_field('youtube_id'); ?>" frameborder="0" allowfullscreen></iframe>
         <?php endif; ?>
       </div>
-      <h1>
-        <?php
-          $category = array_values(get_the_terms($post -> ID , 'show_category'))[0];
-          echo $category -> name;
-        ?>
-      </h1>
 
-      <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-      <p><?php the_field('description'); ?></p>
+      <?php $category = array_values(get_the_terms($post -> ID , 'show_category'))[0]; ?>
 
-      <p><?php the_field('thumbnail'); ?></p>
-
-      <?php if(get_field('credits')): ?>
-        <?php while(has_sub_field('credits')): ?>
-          <div class="credit">
-            <p><?php the_sub_field('credit_type'); ?></p>
-            <p><?php the_sub_field('name'); ?></p>
-            <p><?php the_sub_field('url'); ?></p>
+      <div class="episode-info">
+        <aside class="episode-aside">
+          <a class="channel-link" href="<?php echo get_term_link($category, 'show_category'); ?>">
+            <span class="channel-icon <?php echo $category -> slug ?>"></span>
+          </a>
+        </aside>
+        <div class="episode-main">
+          <h1 class="episode-title"><?php the_title(); ?></h1>
+          <div class="episode-desc">
+            <?php the_field('description'); ?>
           </div>
-        <?php endwhile; ?>
-      <?php endif; ?>
+          <div class="credits">
+            <?php if(get_field('credits')): ?>
+              <?php while(has_sub_field('credits')): ?>
+                <div class="credit">
+                  <p><?php the_sub_field('credit_type'); ?></p>
+                  <p><?php the_sub_field('name'); ?></p>
+                  <p><?php the_sub_field('url'); ?></p>
+                </div>
+              <?php endwhile; ?>
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
     <?php endwhile; ?>
 
     <!--<div class="comments-wrapper">-->
