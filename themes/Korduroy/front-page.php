640f09629f69027621d2f811efcd7aa6ae2b0a07
Paul Tannenbaum <paultannenbaum@gmail.com>
2013-08-24 18:09:23 -0700

    homepage is now pulling in correct data

--- themes/Korduroy/front-page.php
+++ themes/Korduroy/front-page.php
@@ -1,11 +1,51 @@
 <?php get_header(); ?>
 
+<?php
+  $shows = get_posts(array(
+    'post_type'   => 'shows',
+    'orderby'     => 'post_date',
+    'numberposts' => '10'
+  ));
+?>
+
+<?php
+  $blogs = get_posts(array(
+    'post_type'   => 'post',
+    'orderby'     => 'post_date',
+    'numberposts' => '15'
+  ));
+?>
+
+<!-- How to use them in a loop
+  foreach($posts as $post): // begin cycle through posts of this category
+  setup_postdata($post); //set up post data for use in the loop (enables the_title(), etc without specifying a post ID)
+-->
+
 <div id="body" class="home-page">
   <section class="main" role="main">
 
       <div class="featured-content">
         <div class="featured-slider-container">
-          <div class="featured-slider"></div>
+          <div class="featured-slider">
+            <div class="slide slide-1">
+              <?php echo $shows[0]->post_title ?>
+            </div>
+            <div class="slide slide-2">
+              <?php echo $blogs[0]->post_title ?>
+            </div>
+            <div class="slide slide-3">
+              <?php echo $blogs[1]->post_title ?>
+            </div>
+            <div class="slide slide-4">
+              <?php echo $shows[1]->post_title ?>
+            </div>
+            <div class="slide slide-5">
+              <?php echo $blogs[2]->post_title ?>
+            </div>
+            <div class="slide slide-6">
+              <?php echo $blogs[3]->post_title ?>
+            </div>
+          </div>
         </div>
 
         <div class="campaigns-container">
