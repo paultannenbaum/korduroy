56607133732c993b4c02a620317af6d041ece94a
Paul Tannenbaum <paultannenbaum@gmail.com>
2013-09-23 08:46:56 -0700

    small adjustment to shows sub nav

--- themes/Korduroy/partials/shows-sub-nav.php
+++ themes/Korduroy/partials/shows-sub-nav.php
@@ -1,14 +1,18 @@
 <nav id="ktv-sub-nav">
-  <h1 class="sub-nav-heading">Shows</h1>
-  <ul class="sub-nav-list">
-    <?php wp_list_categories(array(
+  <div class="heading-container">
+    <h1 class="sub-nav-heading">Shows</h1>
+  </div>
+  <div class="list-container">
+    <ul class="sub-nav-list">
+      <?php wp_list_categories(array(
       'taxonomy' => 'show_category',
       'hide_empty' => 0,
       'title_li' => '',
       'orderby' => 'count',
       'order' => 'DESC'
-    )); ?>
-  </ul>
+      )); ?>
+    </ul>
+  </div>
   <div class="separator-container">
     <hr class="horizontal-stitch" />
   </div>
