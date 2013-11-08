bf76e8b42556019126b9be4faf65df5b10b91666
Paul Tannenbaum <paultannenbaum@gmail.com>
2013-11-08 13:34:27 -0800

    styling for single product slider

--- themes/Korduroy/woocommerce/single-product/product-thumbnails.php
+++ themes/Korduroy/woocommerce/single-product/product-thumbnails.php
@@ -15,7 +15,7 @@ $attachment_ids = $product->get_gallery_attachment_ids();
 
 if ( $attachment_ids ) {
 	?>
-	<div class="thumbnails"><?php
+	<ul class="thumbnails"><?php
 
 		$loop = 0;
 		$columns = apply_filters( 'woocommerce_product_thumbnails_columns', 3 );
@@ -39,11 +39,11 @@ if ( $attachment_ids ) {
 			$image_class = esc_attr( implode( ' ', $classes ) );
 			$image_title = esc_attr( get_the_title( $attachment_id ) );
 
-			echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '<a href="%s" class="%s" title="%s"  rel="prettyPhoto[product-gallery]">%s</a>', $image_link, $image_class, $image_title, $image ), $attachment_id, $post->ID, $image_class );
+			echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '<li class="product-thumb"><a href="%s" class="%s" title="%s"  rel="prettyPhoto[product-gallery]">%s</a></li>', $image_link, $image_class, $image_title, $image ), $attachment_id, $post->ID, $image_class );
 
 			$loop++;
 		}
 
-	?></div>
+	?></ul>
 	<?php
 }
