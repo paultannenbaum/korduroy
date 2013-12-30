54dbdedebffff85abca683e0ef4f471946123f3c
Paul Tannenbaum <paultannenbaum@gmail.com>
2013-12-30 00:40:00 -0800

    updated google analytics to start tracking

--- themes/Korduroy/partials/third-party-embeds.php
+++ themes/Korduroy/partials/third-party-embeds.php
@@ -1,28 +1,3 @@
-<!--  Facebook
-<div class="social-sharing">
-  <div class="fb-like" data-href="http://developers.facebook.com/docs/reference/plugins/like" data-width="90" data-layout="button_count" data-show-faces="true" data-send="true"></div>
-  <div class="fb-like" data-href="<?php the_permalink() ?>" data-width="90" data-layout="button_count" data-show-faces="true" data-send="true"></div>
-</div>
-
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=598904336828982";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
--->
-
-
-<!--  Twitter
-<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>
-<a href="<?php the_permalink() ?>" class="twitter-share-button" data-lang="en">Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
--->
-
-
-<!-- AddThis -->
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-521959e3746a007e"></script>
 <script type="text/javascript">
   addthis.layers({
@@ -40,12 +15,11 @@
   });
 </script>
 
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<!--<script>-->
-  <!--(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=-->
-    <!--function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;-->
-    <!--e=o.createElement(i);r=o.getElementsByTagName(i)[0];-->
-    <!--e.src='//www.google-analytics.com/analytics.js';-->
-    <!--r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));-->
-  <!--ga('create','UA-XXXXX-X');ga('send','pageview');-->
-<!--</script>-->
+<script>
+  (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+  ga('create','UA-29634704-1');ga('send','pageview');
+</script>
