44c06f8500fe14b1fd5625f1cff2a0dd0f674c05
Paul Tannenbaum <paultannenbaum@gmail.com>
2014-05-17 11:58:41 -0700

    shows by user views query finished

---
+++ themes/Korduroy/templates/test.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Test
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="body" class="shows-page shows-index">
+  <?php
+    // args
+    $args = array(
+      'post_type'		    => 'shows',
+      'posts_per_page'	=> -1,
+      'meta_key'		    => 'user_views',
+      'orderby'		      => 'meta_value_num',
+      'order'			      => 'DESC'
+    );
+
+    // query
+    $wp_query = new WP_Query( $args );
+  ?>
+
+  <?php while ($wp_query->have_posts()) : the_post(); ?>
+    <div><?php the_title(); ?></div>
+  <?php endwhile;?>
+</div>
