1b6ef445acc2dab1c892c4f8d0cceb79efe9126a
Paul Tannenbaum <paultannenbaum@gmail.com>
2014-08-03 19:41:44 -0700

    Updated styling for new nav menu

--- themes/Korduroy/partials/shows-sub-nav.php
+++ themes/Korduroy/partials/shows-sub-nav.php
@@ -4,28 +4,14 @@
   </div>
   <div class="list-container">
     <ul class="shows-sub-nav-list">
-      <li class="cat-item">
+      <li class="cat-item <?php if (is_page('index')) { echo 'current'; } ?>">
         <a href="<?php echo site_url(); ?>/shows/">Recent</a>
       </li>
-      <li class="cat-item <?php if (is_page('most-watched')) { echo "current"; }?>">
+      <li class="cat-item <?php if (is_page('most-watched')) { echo 'current'; } ?>">
         <a href="<?php echo site_url(); ?>/most-watched/">Most Viewed</a>
       </li>
-      <li class="cat-item <?php if (is_page('featured-shows')) { echo "current"; }?>">
+      <li class="cat-item <?php if (is_page('featured-shows')) { echo 'current'; } ?>">
         <a href="<?php echo site_url(); ?>/featured-shows/?show-filter=ktv-favorite">Favorites</a>
-      </li>
-      <li class="drop-down-list-container">
-        <ul class="drop-down-list" data-state="closed">
-          Film Makers
-          <li class="cat-item">
-            <a href="#">Cyrus Sutton</a>
-          </li>
-          <li class="cat-item">
-            <a href="#">Ed Derman</a>
-          </li>
-          <li class="cat-item">
-            <a href="#">Reis Paluso</a>
-          </li>
-        </ul>
       </li>
       <li class="drop-down-list-container">
         <ul class="drop-down-list" data-state="closed">
